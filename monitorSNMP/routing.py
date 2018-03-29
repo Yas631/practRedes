2e6dadd3befe264e2792a834f1a69bb9fcbd4f1a
cris <djcriz5@gmail.com>
2018-03-29 09:57:05 -0600

    graphicas de prueba implementadas

---
+++ monitorSNMP/routing.py
@@ -0,0 +1,8 @@
+# monitorSNMP/routing.py
+from django.conf.urls import url
+
+from . import consumers
+
+websocket_urlpatterns = [
+    url(r'^ws/graph/(?P<room_name>[^/]+)/$', consumers.GraphConsumer),
+]
--- practRedes/settings.py
+++ practRedes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# mysite/settings.py
+# Channels
+ASGI_APPLICATION = 'practRedes.routing.application'
